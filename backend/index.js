4bfb40399d33ef7471276e966e1376dc7e367da0
Shoru <bapcui596@gmail.com>
2024-05-24 23:09:23 +0700

    refactor: moved functions into their respective controller

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,17 @@ const AWS = require("aws-sdk");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const multer = require("multer");
+const {
+  listObjects,
+  uploadFile,
+  editFile,
+  deleteFile,
+  moveFile,
+} = require("./controllers/objectController");
+const {
+  createFolder,
+  deleteFolder,
+} = require("./controllers/folderController");
 
 const app = express();
 app.use(bodyParser.json());
@@ -40,314 +51,75 @@ app.post("/listBuckets", (req, res) => {
   });
 });
 
-app.post("/listObjects", (req, res) => {
-  const { accessKeyId, secretAccessKey, region, bucketName } = req.body;
+app.post("/listObjects", listObjects);
+app.post("/uploadFile", upload.single("file"), uploadFile);
 
-  // Update AWS global configuration
-  AWS.config.update({
-    accessKeyId,
-    secretAccessKey,
-    region,
-  });
+app.post("/createFolder", createFolder);
+const {
+  accessKeyId,
+  secretAccessKey,
+  region,
+  bucketName,
+  oldFolderName,
+  newFolderName,
+} = req.body;
 
-  const s3 = new AWS.S3();
-
-  const params = { Bucket: bucketName };
-  s3.listObjects(params, (err, data) => {
-    if (err) {
-      console.error("Error : ", err);
-      res.status(500).send(err);
-    } else {
-      res.send(data.Contents);
-    }
-  });
+// Update AWS global configuration
+AWS.config.update({
+  accessKeyId,
+  secretAccessKey,
+  region,
 });
 
-app.post("/uploadFile", upload.single("file"), (req, res) => {
-  const { accessKeyId, secretAccessKey, region, bucketName } = req.body;
-  const file = req.file;
+const s3 = new AWS.S3();
 
-  // Update AWS global configuration
-  AWS.config.update({
-    accessKeyId,
-    secretAccessKey,
-    region,
-  });
+// List all objects in the old folder
+const listParams = {
+  Bucket: bucketName,
+  Prefix: oldFolderName,
+};
 
-  const s3 = new AWS.S3();
-
-  const params = {
-    Bucket: bucketName,
-    Key: file.originalname,
-    Body: file.buffer,
-  };
-
-  s3.upload(params, (err, data) => {
-    if (err) {
-      console.error("Error : ", err);
-      res.status(500).send(err);
-    } else {
-      res.send(data);
-    }
-  });
-});
-
-app.post("/createFolder", (req, res) => {
-  const { accessKeyId, secretAccessKey, region, bucketName, folderName } =
-    req.body;
-
-  // Update AWS global configuration
-  AWS.config.update({
-    accessKeyId,
-    secretAccessKey,
-    region,
-  });
-
-  const s3 = new AWS.S3();
-
-  const params = {
-    Bucket: bucketName,
-    Key: `${folderName}/`,
-    Body: "",
-  };
-
-  s3.putObject(params, (err, data) => {
-    if (err) {
-      console.error("Error : ", err);
-      res.status(500).send(err);
-    } else {
-      res.send(data);
-    }
-  });
-});
-app.post("/editFolder", (req, res) => {
-  const {
-    accessKeyId,
-    secretAccessKey,
-    region,
-    bucketName,
-    oldFolderName,
-    newFolderName,
-  } = req.body;
-
-  // Update AWS global configuration
-  AWS.config.update({
-    accessKeyId,
-    secretAccessKey,
-    region,
-  });
-
-  const s3 = new AWS.S3();
-
-  // List all objects in the old folder
-  const listParams = {
-    Bucket: bucketName,
-    Prefix: oldFolderName,
-  };
-
-  s3.listObjects(listParams, (err, data) => {
-    if (err) {
-      console.error("Error : ", err);
-      res.status(500).send(err);
-    } else {
-      const objects = data.Contents;
-      const copyPromises = objects.map((object) => {
-        const newKey = object.Key.replace(oldFolderName, newFolderName);
-        const copyParams = {
-          Bucket: bucketName,
-          CopySource: `${bucketName}/${object.Key}`,
-          Key: newKey,
-        };
-        return s3.copyObject(copyParams).promise();
-      });
-
-      Promise.all(copyPromises)
-        .then(() => {
-          const deletePromises = objects.map((object) => {
-            const deleteParams = {
-              Bucket: bucketName,
-              Key: object.Key,
-            };
-            return s3.deleteObject(deleteParams).promise();
-          });
-
-          return Promise.all(deletePromises);
-        })
-        .then(() => res.send({ message: "Folder renamed successfully" }))
-        .catch((err) => {
-          console.error("Error : ", err);
-          res.status(500).send(err);
-        });
-    }
-  });
-});
-
-app.post("/deleteFolder", (req, res) => {
-  const { accessKeyId, secretAccessKey, region, bucketName, folderName } =
-    req.body;
-
-  // Update AWS global configuration
-  AWS.config.update({
-    accessKeyId,
-    secretAccessKey,
-    region,
-  });
-
-  const s3 = new AWS.S3();
-
-  // List all objects in the folder
-  const listParams = {
-    Bucket: bucketName,
-    Prefix: folderName,
-  };
-
-  s3.listObjects(listParams, (err, data) => {
-    if (err) {
-      console.error("Error : ", err);
-      res.status(500).send(err);
-    } else {
-      const objects = data.Contents;
-      const deletePromises = objects.map((object) => {
-        const deleteParams = {
-          Bucket: bucketName,
-          Key: object.Key,
-        };
-        return s3.deleteObject(deleteParams).promise();
-      });
-
-      Promise.all(deletePromises)
-        .then(() => res.send({ message: "Folder deleted successfully" }))
-        .catch((err) => {
-          console.error("Error : ", err);
-          res.status(500).send(err);
-        });
-    }
-  });
-});
-app.post("/editFile", (req, res) => {
-  const {
-    accessKeyId,
-    secretAccessKey,
-    region,
-    bucketName,
-    oldFileName,
-    newFileName,
-  } = req.body;
-
-  // Update AWS global configuration
-  AWS.config.update({
-    accessKeyId,
-    secretAccessKey,
-    region,
-  });
-
-  const s3 = new AWS.S3();
-
-  // Copy the file to new name
-  const copyParams = {
-    Bucket: bucketName,
-    CopySource: `${bucketName}/${oldFileName}`,
-    Key: newFileName,
-  };
-
-  s3.copyObject(copyParams, (err, data) => {
-    if (err) {
-      console.error("Error : ", err);
-      res.status(500).send(err);
-    } else {
-      // Delete the old file
-      const deleteParams = {
+s3.listObjects(listParams, (err, data) => {
+  if (err) {
+    console.error("Error : ", err);
+    res.status(500).send(err);
+  } else {
+    const objects = data.Contents;
+    const copyPromises = objects.map((object) => {
+      const newKey = object.Key.replace(oldFolderName, newFolderName);
+      const copyParams = {
         Bucket: bucketName,
-        Key: oldFileName,
+        CopySource: `${bucketName}/${object.Key}`,
+        Key: newKey,
       };
+      return s3.copyObject(copyParams).promise();
+    });
 
-      s3.deleteObject(deleteParams, (err, data) => {
-        if (err) {
-          console.error("Error : ", err);
-          res.status(500).send(err);
-        } else {
-          res.send(data);
-        }
+    Promise.all(copyPromises)
+      .then(() => {
+        const deletePromises = objects.map((object) => {
+          const deleteParams = {
+            Bucket: bucketName,
+            Key: object.Key,
+          };
+          return s3.deleteObject(deleteParams).promise();
+        });
+
+        return Promise.all(deletePromises);
+      })
+      .then(() => res.send({ message: "Folder renamed successfully" }))
+      .catch((err) => {
+        console.error("Error : ", err);
+        res.status(500).send(err);
       });
-    }
-  });
+  }
 });
 
-app.post("/deleteFile", (req, res) => {
-  const { accessKeyId, secretAccessKey, region, bucketName, fileName } =
-    req.body;
+app.post("/deleteFolder", deleteFolder);
+app.post("/editFile", editFile);
 
-  // Update AWS global configuration
-  AWS.config.update({
-    accessKeyId,
-    secretAccessKey,
-    region,
-  });
+app.post("/deleteFile", deleteFile);
 
-  const s3 = new AWS.S3();
-
-  const params = {
-    Bucket: bucketName,
-    Key: fileName,
-  };
-
-  s3.deleteObject(params, (err, data) => {
-    if (err) {
-      console.error("Error : ", err);
-      res.status(500).send(err);
-    } else {
-      res.send(data);
-    }
-  });
-});
-
-app.post("/moveFile", (req, res) => {
-  const {
-    accessKeyId,
-    secretAccessKey,
-    region,
-    bucketName,
-    oldFilePath,
-    newFilePath,
-  } = req.body;
-
-  // Update AWS global configuration
-  AWS.config.update({
-    accessKeyId,
-    secretAccessKey,
-    region,
-  });
-
-  const s3 = new AWS.S3();
-
-  // Copy the file to new path
-  const copyParams = {
-    Bucket: bucketName,
-    CopySource: `${bucketName}/${oldFilePath}`,
-    Key: newFilePath,
-  };
-
-  s3.copyObject(copyParams, (err, data) => {
-    if (err) {
-      console.error("Error : ", err);
-      res.status(500).send(err);
-    } else {
-      // Delete the old file
-      const deleteParams = {
-        Bucket: bucketName,
-        Key: oldFilePath,
-      };
-
-      s3.deleteObject(deleteParams, (err, data) => {
-        if (err) {
-          console.error("Error : ", err);
-          res.status(500).send(err);
-        } else {
-          res.send(data);
-        }
-      });
-    }
-  });
-});
+app.post("/moveFile", moveFile);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
