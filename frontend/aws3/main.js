cf1c8ab3392d4cb4faa765b055b19ee30967d9c0
Shoru <bapcui596@gmail.com>
2024-06-16 00:31:54 +0700

    chore: electron setup

---
+++ frontend/aws3/main.js
@@ -0,0 +1,33 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { fileURLToPath } from "url";
+import isDev from "electron-is-dev";
+
+// Create __dirname equivalent for ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1376,
+    height: 768,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  if (isDev) {
+    win.loadURL("http://localhost:3000");
+  } else {
+    win.loadFile(path.join(__dirname, "dist", "index.html"));
+  }
+}
+
+app.on("ready", createWindow);
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
